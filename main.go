f2fc73cc4165e498d064f8e87e3ae0efc805954d
Don Athalage <dknathalage@gmail.com>
2022-11-22 22:54:09 +1100

    change file

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v3"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+
+	yfile, err := ioutil.ReadFile("resume.yml")
+
+	if err != nil {
+
+		log.Fatal(err)
+	}
+
+	data := make(map[interface{}]interface{})
+
+	err2 := yaml.Unmarshal(yfile, &data)
+
+	if err2 != nil {
+
+		log.Fatal(err2)
+	}
+
+	for k, v := range data {
+
+		fmt.Printf("%s -> %d\n", k, v)
+	}
 }
